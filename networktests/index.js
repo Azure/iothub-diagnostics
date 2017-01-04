783c6593881fef83e303998c4981de61d2a930ef
Pierre Cauchois <pierreca@microsoft.com>
2017-01-03 18:49:47 -0800

    Change ping package to avoid relying on node-gyp and requiring admin privileges

--- networktests/index.js
+++ networktests/index.js
@@ -4,20 +4,7 @@ const logger = require('../lib').logger;
 const config = require('../config');
 const dns = require('dns');
 const https = require('https');
-const ping = require('net-ping');
-
-function traceRouteStepCb (err, target, ttl, sent, rcvd) {
-  var ms = rcvd - sent;
-  if (err) {
-      if (err instanceof ping.TimeExceededError) {
-          logger.debug(target + ': ' + err.source + ' (ttl=' + ttl + ' ms=' + ms +')');
-      } else {
-          logger.debug(target + ': ' + err.toString () + ' (ttl=' + ttl + ' ms=' + ms +')');
-      }
-  } else {
-      logger.debug(target + ': ' + target + ' (ttl=' + ttl + ' ms=' + ms +')');
-  }
-}
+const ping = require('ping');
 
 function httpsRequest(done) {
   logger.info('');
@@ -43,45 +30,15 @@ function httpsRequest(done) {
 function pingIpv4Address(ipv4Address, done) {
   logger.info('');
   logger.info("Pinging IPV4 address '" + ipv4Address + "'...");
-
-  const options = {
-      networkProtocol: ping.NetworkProtocol.IPv4,
-      packetSize: 16,
-      retries: 1,
-      sessionId: (process.pid % 65535),
-      timeout: 2000,
-      ttl: 128
-  };
-
- var session = ping.createSession (options);
-  session.on ('error', function (err) {
-      logger.crit(err.toString ());
-      session.close ();
-      return done(err)
-  });
-
-  session.pingHost (ipv4Address, function (error, target) {
-    if (error) {
-      logger.crit ('--> Failed to ping ' + target + ': ' + error.toString ());
-    } else { 
-      logger.info('--> Successfully pinged ' + target);
+  ping.sys.probe(ipv4Address, function(isAlive) {
+    if (isAlive) {
+      logger.info('--> Successfully pinged ' + ipv4Address);
+      return httpsRequest(done);
+    } else {
+      logger.crit ('--> Failed to ping ' + ipv4Address);
     }
-
-    logger.info('');
-    logger.info('Starting TraceRoute to ' + target + '...');
-
-    session.traceRoute (ipv4Address, 100, traceRouteStepCb, function(err) {
-      if(err) {
-        logger.crit('--> Error running traceRoute: ' + err);
-      } else {
-        logger.info('--> Traceroute completed.');
-      } 
-
-      session.close();
-      return httpsRequest(done);    
-    });
   });
-};
+}
 
 function run(done) { 
   var domain = config.pingUrl;
