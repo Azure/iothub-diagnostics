f05bb803dbf6e7fb083d1a234f801087d33be6ba
Pierre Cauchois <pierreca@microsoft.com>
2017-01-19 17:48:46 -0800

    Configure to act as a proper command line utility when installed globally and add copyright notices

--- iothub-diagnostics.js
+++ iothub-diagnostics.js
@@ -1,3 +1,7 @@
+#!/usr/bin/env node
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
 'use strict'
 
 const logger = require('./lib').logger;
